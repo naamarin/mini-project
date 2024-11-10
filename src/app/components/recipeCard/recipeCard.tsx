1c361f05e7b4f16472ef3144566978244f6729f7
TamariIv <taivgi@g.jct.ac.il>
2024-11-10 15:07:41 +0200

    recipe cards render ok

--- src/app/components/recipeCard/recipeCard.tsx
+++ src/app/components/recipeCard/recipeCard.tsx
@@ -1,6 +1,7 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './recipeCard.module.css';
+import { FaRegStar, FaStar } from "react-icons/fa";
 
 
 // types.ts
@@ -14,16 +15,26 @@ export type Recipe = {
 };
 
 
-function RecipeCard(recipe: Recipe) {
-    console.log(recipe, );
+function RecipeCard({ recipe }: { recipe: Recipe }) {
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const toggleFavorite = () => {
+        setIsFavorite(!isFavorite);
+    };
+
     return (
         <div className={styles.card}>
 
             <img src={recipe.image} />
             <div className={styles.cardContent}>
-                <p className={styles.cardTitle}>{recipe.name}</p>
-                <p className={styles.cardTitle}>{recipe.category}</p>
+                <p className={styles.cardName}>{recipe.name}</p>
+                <p className={styles.cardCategory}>{recipe.category}</p>
                 <p className={styles.cardDescription}>{recipe.instructions}</p>
+                <button className={styles.infoButton}>More Info</button>
+                <button onClick={toggleFavorite}>
+                    {isFavorite ? <FaRegStar className={styles.starIcon} /> : <FaStar className={styles.starIcon} />}
+                </button>
+
             </div>
         </div>
     );
