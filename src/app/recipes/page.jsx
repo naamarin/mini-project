9075989c90e4a352fb55c08e332eee07aa30c6dc
vhadas <h56781602@gmail.com>
2024-11-11 12:15:35 +0200

    Remove-Item -Recurse -Force .next

--- src/app/recipes/page.jsx
+++ src/app/recipes/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import styles from './recipes.module.css';
-import RecipeCard from '../components/recipeCard/recipeCard';
+import RecipeCard from '@/components/recipeCard/recipeCard';
 
 function Recipes() {
     const [recipes,setRecipes] = useState([]);
