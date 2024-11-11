ffb470a89875a2138f3efbaefdec14f70f0b3395
Naama Iluz <naamarin@g.jct.ac.il>
2024-11-11 11:09:21 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 
 export default function Home() {
+  
   return (
     <header style={{ padding: '20px', backgroundColor: '#f8f9fa', borderBottom: '1px solid #ccc' }}>
-        <h1>Recipes</h1>
-        <nav style={{ display: 'flex', gap: '20px' }}>
-          <Link href="/recipes">All Recipes</Link>
-          <Link href="/favorites">Favorites</Link>
-        </nav>
-      </header>
+      <h1>Recipes</h1>
+      <nav style={{ display: 'flex', gap: '20px' }}>
+        <Link href="/recipes">All Recipes</Link>
+        <Link href="/favorites">Favorites</Link>
+      </nav>
+    </header>
   );
 }
