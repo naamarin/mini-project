e5bf5905f09590c4865e52858d935020c8404380
ahuva12 <ahuvaeshit@gmail.com>
2024-11-11 11:30:16 +0200

    css form

--- src/components/FromRecipe/FormRecipe.tsx
+++ src/components/FromRecipe/FormRecipe.tsx
@@ -46,104 +46,93 @@ export default function AddRecipe() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label>Meal name:</label>
-        <input type="text" {...register("recipeName")} />
-        {errors.recipeName && (
-          <p style={{ color: "red" }}>{errors.recipeName.message}</p>
-        )}
+        <label className={styles.label}>
+          <input className={styles.input} type="text" {...register("recipeName")} placeholder=" " />
+          <span>Meal name</span>
+        </label>
+        {errors.recipeName && <p className={styles.errorMessage}>{errors.recipeName.message}</p>}
       </div>
 
       <div>
-        <label>Category:</label>
-        <select {...register("category")}>
-          <option value="Breakfest">Breakfest</option>
-          <option value="Pasta">Pasta</option>
-          <option value="Salad">Salad</option>
-          <option value="Main course">Main course</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-        {errors.category && (
-          <p style={{ color: "red" }}>{errors.category.message}</p>
-        )}
+        <label className={styles.label}>
+          <select className={styles.input} {...register("category")}>
+            <option value="" disabled selected>Category</option>
+            <option value="Breakfast">Breakfast</option>
+            <option value="Pasta">Pasta</option>
+            <option value="Salad">Salad</option>
+            <option value="Main course">Main course</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </label>
+        {errors.category && <p className={styles.errorMessage}>{errors.category.message}</p>}
       </div>
 
       <div>
-        <label>Image URL:</label>
-        <input type="text" {...register("imageUrl")} />
-        {errors.imageUrl && (
-          <p style={{ color: "red" }}>{errors.imageUrl.message}</p>
-        )}
+        <label className={styles.label}>
+          <input className={styles.input} type="text" {...register("imageUrl")} placeholder=" " />
+          <span>Image URL</span>
+        </label>
+        {errors.imageUrl && <p className={styles.errorMessage}>{errors.imageUrl.message}</p>}
       </div>
 
-      <div className={styles.Ingredients}>
-        <label>Ingredients</label>
+      <div className={styles.ingredients}>
+        <label className={styles.label}>
+          <span>Ingredients</span>
+        </label>
         {fields.map((field, index) => (
-          <div key={field.id}>
+          <div key={field.id} className={styles.flex}>
             <Controller
               control={control}
               name={`ingredients.${index}.name`}
               render={({ field }) => (
-                <input
-                  type="text"
-                  {...field}
-                  placeholder="Ingredient name"
-                />
+                <input className={styles.input} type="text" {...field} placeholder="name" />
               )}
             />
             {errors.ingredients?.[index]?.name && (
-              <p style={{ color: "red" }}>
-                {errors.ingredients[index].name?.message}
-              </p>
+              <p className={styles.errorMessage}>{errors.ingredients[index].name?.message}</p>
             )}
-
-            <Controller 
+            <Controller
               control={control}
               name={`ingredients.${index}.quantity`}
               render={({ field }) => (
-                <input
-                  type="text"
-                  {...field}
-                  placeholder="Ingredient quantity"
-                />
+                <input className={styles.input} type="text" {...field} placeholder="quantity" />
               )}
             />
             {errors.ingredients?.[index]?.quantity && (
-              <p style={{ color: "red" }}>
-                {errors.ingredients[index].quantity?.message}
-              </p>
+              <p className={styles.errorMessage}>{errors.ingredients[index].quantity?.message}</p>
             )}
-
             {index === fields.length - 1 && (
-              <button type="button" onClick={() => append({ name: "", quantity: "" })}>
-                +
+              <button type="button" onClick={() => append({ name: "", quantity: "" })} className={styles.fancy}>
+                <span className={styles.text}>+</span>
+                <span className={styles.topKey}></span>
+                <span className={styles.bottomKey1}></span>
+                <span className={styles.bottomKey2}></span>
               </button>
             )}
           </div>
         ))}
-        {errors.ingredients && (
-          <p style={{ color: "red" }}>{errors.ingredients.message}</p>
-        )}
       </div>
 
-      <div className={styles.Instructions}>
-        <label>Instructions</label>
-        <textarea {...register("instructions")} />
-        {errors.instructions && (
-          <p style={{ color: "red" }}>{errors.instructions.message}</p>
-        )}
+      <div className={styles.instructions}>
+        <label className={styles.label}>
+          <textarea className={styles.input} {...register("instructions")} placeholder=" " />
+          <span className={styles.Instructions}>Instructions</span>
+        </label>
+        {errors.instructions && <p className={styles.errorMessage}>{errors.instructions.message}</p>}
       </div>
 
-      <div>
-        <button
-          type="button"
-          onClick={() => (window.location.href = "../recipes")}
-        >
-          Back
+      <div className={styles.flex}>
+        <button type="button" className={styles.fancy} onClick={() => (window.location.href = "../recipes")}>
+          <span className={styles.text}>Back</span>
         </button>
-        <button type="submit">Add</button>
+        <button type="submit" className={styles.fancy}>
+          <span className={styles.text}>Add</span>
+        </button>
       </div>
     </form>
   );
 }
+
+
