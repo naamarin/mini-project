00725258a47281804d1b925ba42751fe77962a92
TamariIv <taivgi@g.jct.ac.il>
2024-11-12 09:45:44 +0200

    fix filtering

--- src/services/types.ts
+++ src/services/types.ts
@@ -2,12 +2,12 @@ import { ObjectId } from "mongodb";
 
 export interface Recipe {
     _id: ObjectId;
-    nameRcipe: string;
+    nameRecipe: string;
     category: string;
-    image: number;
-    ingredients:  Array<{ name: string; quantity: string }>;
+    image: string;
+    ingredients: Array<{ name: string; quantity: string }>;
     preparationInstructions: string;
 }
 
 
-  
+
