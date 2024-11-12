50d4695a284dccb6cfbc8f88b03ada58b487542e
TamariIv <taivgi@g.jct.ac.il>
2024-11-12 11:47:59 +0200

    fix favorite render

--- src/components/recipeCard/recipeCard.tsx
+++ src/components/recipeCard/recipeCard.tsx
@@ -4,25 +4,11 @@ import styles from './recipeCard.module.css';
 import { FaRegStar, FaStar } from "react-icons/fa";
 import { Recipe } from "../../services/types"
 
-// types.ts
-// export type Recipe = {
-//     id: string;
-//     name: string;
-//     category: string;
-//     image: string; // URL as a string
-//     ingredients: string[]; // Array of ingredient strings
-//     instructions: string; // Instructions as a single string, or array if multiple steps
-// };
 
 function RecipeCard({ recipe }: { recipe: Recipe }) {
-    const [isFavorite, setIsFavorite] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(Boolean(localStorage.getItem(`${recipe._id}-is-favorite`)));
 
     const toggleFavorite = () => {
-        // const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
-        // console.log(favorites);
-        // favorites.push(recipe.id);
-        // console.log(favorites);
-        // localStorage.setItem('favorites', favorites);
         localStorage.setItem(`${recipe._id}-is-favorite`, JSON.stringify(!isFavorite));
         setIsFavorite(!isFavorite);
     };
