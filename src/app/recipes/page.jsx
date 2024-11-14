e8429ddd55793ac5e54842f609663c054208b893
TamariIv <taivgi@g.jct.ac.il>
2024-11-14 11:09:15 +0200

    remove dups

--- src/app/recipes/page.jsx
+++ src/app/recipes/page.jsx
@@ -10,160 +10,139 @@ import categoriesStore from "@/store/categoriesStore";
 import Pagination from "../../components/Pagination/Pagination";
 
 function Recipes() {
-  const [recipes, setRecipes] = useState([]);
-  const [renderedRecipes, setRenderedRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
-  //  const [error, setError] = useState(null);
-  const [isPopUp, setIsPopUp] = useState(false);
-  const [selectedRecipe, setSelectedRecipe] = useState(null);
-  const { initializeCategories } = categoriesStore();
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 9;
+    const [recipes, setRecipes] = useState([]);
+    const [renderedRecipes, setRenderedRecipes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    //  const [error, setError] = useState(null);
+    const [isPopUp, setIsPopUp] = useState(false);
+    const [selectedRecipe, setSelectedRecipe] = useState(null);
+    const { initializeCategories } = categoriesStore();
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 9;
 
- useEffect(() => {
-   const fetchRecipes = async () => {
-     try {
-       setLoading(true);
-       const data = await getRecipes();
-       setRecipes(data);
-       setRenderedRecipes(data);
-       initializeCategoriesStore(data)
-     }
-     catch (err) {
-       setError('Failed to fetch recipes');
-     }
-     finally {
-       setLoading(false);
-     }
-   };
-   fetchRecipes();
- }, []);
-  
- const initializeCategoriesStore = (recipes) => {
-   const allCategories = [...new Set(recipes.map(recipe => recipe.category))];
-   initializeCategories(allCategories);
- } 
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                setLoading(true);
+                const data = await getRecipes();
+                setRecipes(data);
+                setRenderedRecipes(data);
+                initializeCategoriesStore(data)
+            }
+            catch (err) {
+                setError('Failed to fetch recipes');
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+        fetchRecipes();
+    }, []);
 
-  const filterFavorites = () => {
-      const favorites = renderedRecipes.filter(recipe => localStorage.getItem(`${recipe._id}-is-favorite`) === 'true');
-      setRenderedRecipes(favorites);
-  }
-
-  const filterCategory = (category) => {
-    if (category === 'All') {
-      setRenderedRecipes(recipes);
-    } else {
-      const filtered = recipes.filter(recipe => recipe.category === category);
-      setRenderedRecipes(filtered);
+    const initializeCategoriesStore = (recipes) => {
+        const allCategories = [...new Set(recipes.map(recipe => recipe.category))];
+        initializeCategories(allCategories);
     }
-  };
 
-  const handleSearch = (query) => {
+    const filterCategory = (category) => {
+        if (category === 'All') {
+            setRenderedRecipes(recipes);
+        } else {
+            const filtered = recipes.filter(recipe => recipe.category === category);
+            setRenderedRecipes(filtered);
+        }
+    };
+
+    const handleSearch = (query) => {
         const filtered = recipes.filter(recipe =>
             recipe.nameRecipe.toLowerCase().includes(query.toLowerCase())
         );
         setRenderedRecipes(filtered);
     };
 
-  const filterFavorites = () => {
-    const favorites = renderedRecipes.filter(
-      (recipe) => localStorage.getItem(`${recipe._id}-is-favorite`) === "true"
-    );
-    setRenderedRecipes(favorites);
-  };
+    const filterFavorites = () => {
+        const favorites = renderedRecipes.filter(
+            (recipe) => localStorage.getItem(`${recipe._id}-is-favorite`) === "true"
+        );
+        setRenderedRecipes(favorites);
+    };
 
-  const filterCategory = (category) => {
-    if (category === "All") {
-      setRenderedRecipes(recipes);
-    } else {
-      const filtered = recipes.filter((recipe) => recipe.category === category);
-      setRenderedRecipes(filtered);
-    }
-  };
+    const allRecipes = () => {
+        setRenderedRecipes(recipes);
+    };
 
-  const handleSearch = (query) => {
-    const filtered = recipes.filter((recipe) =>
-      recipe.nameRecipe.toLowerCase().includes(query.toLowerCase())
-    );
-    setRenderedRecipes(filtered);
-  };
+    // Pagination logic
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = renderedRecipes.slice(indexOfFirstItem, indexOfLastItem);
 
-  const allRecipes = () => {
-    setRenderedRecipes(recipes);
-  };
+    const totalPages = Math.ceil(renderedRecipes.length / itemsPerPage);
 
-  // Pagination logic
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = renderedRecipes.slice(indexOfFirstItem, indexOfLastItem);
+    const goToNextPage = () => {
+        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    };
 
-  const totalPages = Math.ceil(renderedRecipes.length / itemsPerPage);
+    const goToPreviousPage = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    };
 
-  const goToNextPage = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
-  };
+    return (
+        <div>
+            <div className={styles.header}>
+                <h1 className={styles.title}>Recipes</h1>
+                <RecipesHeader
+                    className={styles.recipesHeader}
+                    onSelect={filterCategory}
+                    onSearch={handleSearch}
+                />
+            </div>
+            <div className={styles.wrapper}>
+                <div className={styles.filterComponents}>
+                    <div className={styles.filter} onClick={filterFavorites}>
+                        Favorites
+                    </div>
+                    <div className={styles.filter} onClick={allRecipes}>
+                        All
+                    </div>
+                </div>
+                <Link href="/addRecipe">
+                    <button className={styles.addButton}>Add</button>
+                </Link>
+            </div>
 
-  const goToPreviousPage = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-  };
+            {loading ? (
+                <>
+                    <h1 style={{ marginLeft: "4rem" }}>Loading...</h1>
+                </>
+            ) : (
+                <>
+                    {isPopUp && selectedRecipe && (
+                        <PopUpRecipe recipe={selectedRecipe} setIsPopUp={setIsPopUp} />
+                    )}
+                    <div className={styles.recipes}>
+                        {currentItems.map((recipe, index) => (
+                            <RecipeCard
+                                key={index}
+                                recipe={recipe}
+                                setIsPopUp={setIsPopUp}
+                                setSelectedRecipe={setSelectedRecipe}
+                            />
+                        ))}
+                    </div>
 
-  return (
-    <div>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Recipes</h1>
-        <RecipesHeader
-          className={styles.recipesHeader}
-          onSelect={filterCategory}
-          onSearch={handleSearch}
-        />
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.filterComponents}>
-          <div className={styles.filter} onClick={filterFavorites}>
-            Favorites
-          </div>
-          <div className={styles.filter} onClick={allRecipes}>
-            All
-          </div>
+                    <Pagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        goToNextPage={goToNextPage}
+                        goToPreviousPage={goToPreviousPage}
+                    />
+                </>
+            )}
+            <footer className={styles.footer}>
+                <p>&copy; {new Date().getFullYear()} All rights reserved.</p>
+            </footer>
         </div>
-        <Link href="/addRecipe">
-          <button className={styles.addButton}>Add</button>
-        </Link>
-      </div>
-
-      {loading ? (
-        <>
-          <h1 style={{ marginLeft: "4rem" }}>Loading...</h1>
-        </>
-      ) : (
-        <>
-          {isPopUp && selectedRecipe && (
-            <PopUpRecipe recipe={selectedRecipe} setIsPopUp={setIsPopUp} />
-          )}
-          <div className={styles.recipes}>
-            {currentItems.map((recipe, index) => (
-              <RecipeCard
-                key={index}
-                recipe={recipe}
-                setIsPopUp={setIsPopUp}
-                setSelectedRecipe={setSelectedRecipe}
-              />
-            ))}
-          </div>
-
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            goToNextPage={goToNextPage}
-            goToPreviousPage={goToPreviousPage}
-          />
-        </>
-      )}
-      <footer className={styles.footer}>
-        <p>&copy; {new Date().getFullYear()} All rights reserved.</p>
-      </footer>
-    </div>
-  );
+    );
 }
 
 export default Recipes;
